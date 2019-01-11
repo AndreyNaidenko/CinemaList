932c1ce5e9240b4f97e9c668e6a4eccd236214dd
Andrey Naidenko <naidenkoandrey1997@mail.ru>
2019-01-11 15:31:11 +0400

    Create models, admin. Registered models.

--- app_cinema_list/admin.py
+++ app_cinema_list/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cinema
+from .models import Actor
+from .models import Genre
 
-# Register your models here.
+
+admin.site.register(Cinema)
+admin.site.register(Actor)
+admin.site.register(Genre)
--- app_cinema_list/models.py
+++ app_cinema_list/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Cinema(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    year = models.IntegerField(blank=True)
+    rating = models.DecimalField(max_digits=10, decimal_places=6, blank=True)
+
+
+class Actor(models.Model):
+    cinema_id = models.ForeignKey(
+        'Cinema',
+        on_delete=models.CASCADE,
+        blank=True
+    )
+    name = models.CharField(max_length=50, blank=True)
+    birth_date = models.DateField(blank=True)
+
+
+class Genre(models.Model):
+    cinema_id = models.ForeignKey(
+        'Cinema',
+        on_delete=models.CASCADE,
+        blank=True
+    )
+    name = models.CharField(max_length=30, blank=True)
